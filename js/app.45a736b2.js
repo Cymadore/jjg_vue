46ddc173ac0707e232c44ad0fa85b801b7feeb7b
cymadore <jjg0111@naver.com>
2024-10-24 12:08:27 +0900

    Updates

---
+++ js/app.45a736b2.js
@@ -0,0 +1,399 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 6483:
+/***/ (function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+
+
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-dom/dist/runtime-dom.esm-bundler.js
+var runtime_dom_esm_bundler = __webpack_require__(5130);
+// EXTERNAL MODULE: ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js
+var runtime_core_esm_bundler = __webpack_require__(6768);
+// EXTERNAL MODULE: ./node_modules/@vue/shared/dist/shared.esm-bundler.js
+var shared_esm_bundler = __webpack_require__(4232);
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/App.vue?vue&type=template&id=5889c7b6
+
+const _hoisted_1 = {
+  id: "nav"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_router_view = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("router-view");
+  return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createElementBlock */.CE)(runtime_core_esm_bundler/* Fragment */.FK, null, [(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", _hoisted_1, [(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+    class: (0,shared_esm_bundler/* normalizeClass */.C4)(["nav-bar", {
+      'nav-bar-hidden': $setup.menuhidden
+    }])
+  }, _cache[0] || (_cache[0] = [(0,runtime_core_esm_bundler/* createStaticVNode */.Fv)("<div class=\"bar-content\"><a href=\"#main\">home</a><a href=\"#skills\">skills</a><a href=\"#courier\">courier</a><a href=\"#project\">project</a></div>", 1)]), 2)]), (0,runtime_core_esm_bundler/* createVNode */.bF)(_component_router_view)], 64);
+}
+;// ./src/App.vue?vue&type=template&id=5889c7b6
+
+// EXTERNAL MODULE: ./node_modules/@vue/reactivity/dist/reactivity.esm-bundler.js
+var reactivity_esm_bundler = __webpack_require__(144);
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/App.vue?vue&type=script&lang=js
+
+/* harmony default export */ var Appvue_type_script_lang_js = ({
+  setup() {
+    const menuhidden = (0,reactivity_esm_bundler/* ref */.KR)(false);
+    function onScroll() {
+      const currentScroll = document.documentElement.scrollTop;
+      if (currentScroll <= 80) {
+        menuhidden.value = false;
+      } else {
+        menuhidden.value = true;
+      }
+    }
+    (0,runtime_core_esm_bundler/* onMounted */.sV)(() => {
+      console.log('onmounted');
+      window.addEventListener("scroll", onScroll);
+    });
+    (0,runtime_core_esm_bundler/* onBeforeUnmount */.xo)(() => {
+      console.log('onBeforeUnmount');
+      window.removeEventListener("scroll", onScroll);
+    });
+    return {
+      menuhidden
+    };
+  }
+});
+;// ./src/App.vue?vue&type=script&lang=js
+ 
+;// ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-22.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-22.use[1]!./node_modules/vue-loader/dist/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-22.use[2]!./node_modules/sass-loader/dist/cjs.js??clonedRuleSet-22.use[3]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/App.vue?vue&type=style&index=0&id=5889c7b6&lang=scss
+// extracted by mini-css-extract-plugin
+
+;// ./src/App.vue?vue&type=style&index=0&id=5889c7b6&lang=scss
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(1241);
+;// ./src/App.vue
+
+
+
+
+;
+
+
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.A)(Appvue_type_script_lang_js, [['render',render]])
+
+/* harmony default export */ var App = (__exports__);
+// EXTERNAL MODULE: ./node_modules/vue-router/dist/vue-router.mjs
+var vue_router = __webpack_require__(1387);
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/ProjectPage.vue?vue&type=template&id=76f0026b
+
+const ProjectPagevue_type_template_id_76f0026b_hoisted_1 = {
+  id: "project",
+  class: "project"
+};
+function ProjectPagevue_type_template_id_76f0026b_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createElementBlock */.CE)("div", ProjectPagevue_type_template_id_76f0026b_hoisted_1, _cache[0] || (_cache[0] = [(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+    class: "contents"
+  }, " PlayTable Portfolio ", -1)]));
+}
+;// ./src/views/ProjectPage.vue?vue&type=template&id=76f0026b
+
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/ProjectPage.vue?vue&type=script&lang=js
+/* harmony default export */ var ProjectPagevue_type_script_lang_js = ({});
+;// ./src/views/ProjectPage.vue?vue&type=script&lang=js
+ 
+;// ./src/views/ProjectPage.vue
+
+
+
+
+;
+const ProjectPage_exports_ = /*#__PURE__*/(0,exportHelper/* default */.A)(ProjectPagevue_type_script_lang_js, [['render',ProjectPagevue_type_template_id_76f0026b_render]])
+
+/* harmony default export */ var ProjectPage = (ProjectPage_exports_);
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/SkillsPage.vue?vue&type=template&id=40956514
+
+const SkillsPagevue_type_template_id_40956514_hoisted_1 = {
+  id: "skills",
+  class: "skills"
+};
+function SkillsPagevue_type_template_id_40956514_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createElementBlock */.CE)("div", SkillsPagevue_type_template_id_40956514_hoisted_1, _cache[0] || (_cache[0] = [(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+    class: "contents"
+  }, " 프론트엔드 HTML5 CSS3 TailwindCSS JavaScript React Vue.js Next.js ", -1)]));
+}
+;// ./src/views/SkillsPage.vue?vue&type=template&id=40956514
+
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/SkillsPage.vue?vue&type=script&lang=js
+/* harmony default export */ var SkillsPagevue_type_script_lang_js = ({});
+;// ./src/views/SkillsPage.vue?vue&type=script&lang=js
+ 
+;// ./src/views/SkillsPage.vue
+
+
+
+
+;
+const SkillsPage_exports_ = /*#__PURE__*/(0,exportHelper/* default */.A)(SkillsPagevue_type_script_lang_js, [['render',SkillsPagevue_type_template_id_40956514_render]])
+
+/* harmony default export */ var SkillsPage = (SkillsPage_exports_);
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/IndexPage.vue?vue&type=template&id=c54a8914
+
+function IndexPagevue_type_template_id_c54a8914_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_SkillsPage = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("SkillsPage");
+  const _component_CourierPage = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("CourierPage");
+  const _component_ProjectPage = (0,runtime_core_esm_bundler/* resolveComponent */.g2)("ProjectPage");
+  return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createElementBlock */.CE)(runtime_core_esm_bundler/* Fragment */.FK, null, [_cache[0] || (_cache[0] = (0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+    id: "main",
+    class: "main"
+  }, [(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+    class: "contents"
+  }, " 장재근 프론트 엔드 개발자 안녕하세요. 4년차 프론트엔드 개발자 장재근입니다. ")], -1)), (0,runtime_core_esm_bundler/* createVNode */.bF)(_component_SkillsPage), (0,runtime_core_esm_bundler/* createVNode */.bF)(_component_CourierPage), (0,runtime_core_esm_bundler/* createVNode */.bF)(_component_ProjectPage)], 64);
+}
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[3]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/CourierPage.vue?vue&type=template&id=0b3b1e88
+
+const CourierPagevue_type_template_id_0b3b1e88_hoisted_1 = {
+  id: "courier",
+  class: "courier"
+};
+function CourierPagevue_type_template_id_0b3b1e88_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return (0,runtime_core_esm_bundler/* openBlock */.uX)(), (0,runtime_core_esm_bundler/* createElementBlock */.CE)("div", CourierPagevue_type_template_id_0b3b1e88_hoisted_1, _cache[0] || (_cache[0] = [(0,runtime_core_esm_bundler/* createElementVNode */.Lk)("div", {
+    class: "contents"
+  }, " 주식회사 스페서 (2024.03~2024.05) 사원 매장 공동 예약 플랫폼 개발 참여 ", -1)]));
+}
+;// ./src/views/CourierPage.vue?vue&type=template&id=0b3b1e88
+
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/CourierPage.vue?vue&type=script&lang=js
+/* harmony default export */ var CourierPagevue_type_script_lang_js = ({});
+;// ./src/views/CourierPage.vue?vue&type=script&lang=js
+ 
+;// ./src/views/CourierPage.vue
+
+
+
+
+;
+const CourierPage_exports_ = /*#__PURE__*/(0,exportHelper/* default */.A)(CourierPagevue_type_script_lang_js, [['render',CourierPagevue_type_template_id_0b3b1e88_render]])
+
+/* harmony default export */ var CourierPage = (CourierPage_exports_);
+;// ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./src/views/IndexPage.vue?vue&type=script&lang=js
+
+
+
+/* harmony default export */ var IndexPagevue_type_script_lang_js = ({
+  components: {
+    CourierPage: CourierPage,
+    SkillsPage: SkillsPage,
+    ProjectPage: ProjectPage
+  }
+});
+;// ./src/views/IndexPage.vue?vue&type=script&lang=js
+ 
+;// ./src/views/IndexPage.vue
+
+
+
+
+;
+const IndexPage_exports_ = /*#__PURE__*/(0,exportHelper/* default */.A)(IndexPagevue_type_script_lang_js, [['render',IndexPagevue_type_template_id_c54a8914_render]])
+
+/* harmony default export */ var IndexPage = (IndexPage_exports_);
+;// ./src/router/index.js
+
+
+
+
+
+const routes = [{
+  path: "/",
+  name: "Index",
+  component: IndexPage
+}, {
+  path: "/skills",
+  name: "Skills",
+  component: SkillsPage
+}, {
+  path: "/courier",
+  name: "Courier",
+  component: CourierPage
+}, {
+  path: "/project",
+  name: "Project",
+  component: ProjectPage
+}];
+const router = (0,vue_router/* createRouter */.aE)({
+  history: (0,vue_router/* createWebHistory */.LA)(),
+  routes
+});
+/* harmony default export */ var src_router = (router);
+;// ./src/main.js
+
+
+
+const app = (0,runtime_dom_esm_bundler/* createApp */.Ef)(App);
+app.use(src_router).mount('#app');
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	!function() {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = function(result, chunkIds, fn, priority) {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var chunkIds = deferred[i][0];
+/******/ 				var fn = deferred[i][1];
+/******/ 				var priority = deferred[i][2];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every(function(key) { return __webpack_require__.O[key](chunkIds[j]); })) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	!function() {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	!function() {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			524: 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = function(chunkId) { return installedChunks[chunkId] === 0; };
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = function(parentChunkLoadingFunction, data) {
+/******/ 			var chunkIds = data[0];
+/******/ 			var moreModules = data[1];
+/******/ 			var runtime = data[2];
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some(function(id) { return installedChunks[id] !== 0; })) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkjjg_vue"] = self["webpackChunkjjg_vue"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [504], function() { return __webpack_require__(6483); })
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
