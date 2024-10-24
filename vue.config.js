714e4fc07cc3d7b8f3949f51f388c7f2983f4317
cymadore <jjg0111@naver.com>
2024-10-24 16:57:11 +0900

    배포 수정

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir:"./docs",
-  publicPath: "/",
+  publicPath: "/jjg_vue/",
   transpileDependencies: true
 })
